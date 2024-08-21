99ddaf624fc05e3fbdae5661700d0d293f91a858
Jarred Moyer <jarredds941@gmail.com>
2024-08-20 21:51:49 -0400

    run time react fixes

--- quest/src/App.js
+++ quest/src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import getData from './Services/GetQuestData';
+import {useEffect, useState} from 'react'
 
 function App() {
 
 
-  const [quests,setQuests] = useReact(null)
-
-  var d = getData();
-  console.log(d)
+  const [quests,setQuests] = useState({})
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await getData();
+      setQuests(data);
+    };
+    fetchData();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
